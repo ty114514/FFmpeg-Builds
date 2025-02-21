7af285d8e595943b2f28700fe799708f29511dfb
Xi Wuuuuuuuuuuuu~~~~~~~~~~~~~~~ <nekomain@outlook.com>
2025-02-21 14:35:17 +0800

    use svt-av1-psy

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
+SCRIPT_REPO="https://github.com/psy-ex/svt-av1-psy.git"
 SCRIPT_COMMIT="master"
 
 ffbuild_enabled() {
@@ -9,7 +9,7 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerdl() {
-    echo "git clone \"$SCRIPT_REPO\" . && git checkout \"$SCRIPT_COMMIT\""
+    echo "git clone \"$SCRIPT_REPO\" . && git checkout \"$SCRIPT_COMMIT\" SVT-AV1"
 }
 
 ffbuild_dockerbuild() {
