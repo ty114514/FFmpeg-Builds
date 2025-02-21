55e92ed812a83f3dd74ece4dc2aeab4e8fa9b2e2
Xi Wuuuuuuuuuuuu~~~~~~~~~~~~~~~ <nekomain@outlook.com>
2025-02-21 12:15:21 +0800

    Update 50-svtav1.sh

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="41c884a69f96a2bef10c668d9790b5969e82735a"
+SCRIPT_COMMIT="master"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
