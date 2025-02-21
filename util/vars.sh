1ed877574c9a299dd9aed77d454ca8c4cfa9eab7
Xi Wuuuuuuuuuuuu~~~~~~~~~~~~~~~ <nekomain@outlook.com>
2025-02-21 12:24:17 +0800

    Update vars.sh

--- util/vars.sh
+++ util/vars.sh
@@ -30,7 +30,7 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-REPO="${GITHUB_REPOSITORY:-btbn/ffmpeg-builds}"
+REPO="btbn/ffmpeg-builds"
 REPO="${REPO,,}"
 REGISTRY="${REGISTRY_OVERRIDE:-ghcr.io}"
 BASE_IMAGE="${REGISTRY}/${REPO}/base:latest"
